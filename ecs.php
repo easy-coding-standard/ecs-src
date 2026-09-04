48b8df19cfb3fad76e682ddf27a1dbaeb4ebad62
Tomas Votruba <tomas.vot@gmail.com>
2026-09-05 00:03:38 +0200

    Deprecate withEditorConfig(), drop EditorConfig support (#68)

--- ecs.php
+++ ecs.php
@@ -6,7 +6,6 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
     ->withPaths([__DIR__ . '/bin', __DIR__ . '/config', __DIR__ . '/packages', __DIR__ . '/src', __DIR__ . '/tests'])
-    ->withEditorConfig()
     ->withRootFiles()
     ->withSkip(['*/Source/*', '*/Fixture/*'])
     ->withPreparedSets(psr12: true, common: true);
